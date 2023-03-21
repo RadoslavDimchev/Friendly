0917e05f084ee9514145cd52a2ff3aba00a7811d
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-03-21 22:59:34 +0200

    Add three rows to User Widget

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -2,29 +2,29 @@ import {
   ManageAccountsOutlined,
   EditOutlined,
   LocationOnOutlined,
-  WorkOutlineOutlined
-} from '@mui/icons-material';
-import {Box, Typography, Divider, useTheme} from '@mui/material';
-import FlexBetween from 'components/FlexBetween';
-import UserImage from 'components/UserImage';
-import WidgetWrapper from 'components/WidgetWrapper';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+  WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const UserWidget = ({userId, picturePath}) => {
+const UserWidget = ({ userId, picturePath }) => {
   const [user, setUser] = useState(null);
-  const {palette} = useTheme();
+  const { palette } = useTheme();
   const navigate = useNavigate();
-  const token = useSelector(state => state.token);
+  const token = useSelector((state) => state.token);
   const dark = palette.neutral.dark;
   const medium = palette.neutral.medium;
   const main = palette.neutral.main;
 
   const getUser = async () => {
     const response = await fetch(`http://localhost:3001/users/${userId}`, {
-      method: 'GET',
-      headers: {Authorization: `Bearer ${token}`},
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
     setUser(data);
@@ -34,11 +34,83 @@ const UserWidget = ({userId, picturePath}) => {
     getUser();
   }, []);
 
-  if(!user) {
+  if (!user) {
     return null;
   }
 
-  return ();
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
+
+  return (
+    <WidgetWrapper>
+      {/* FIRST ROW */}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap="1rem">
+          <UserImage image={picturePath} />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{friends.length}</Typography>
+          </Box>
+          <ManageAccountsOutlined />
+        </FlexBetween>
+        <Divider />
+
+        {/* SECOND ROW */}
+        <Box p="1rem 0">
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{location}</Typography>
+          </Box>
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+            <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{occupation}</Typography>
+          </Box>
+        </Box>
+
+        {/* THIRD ROW */}
+        <Box p="1rem 0">
+          <FlexBetween>
+            <Typography color={medium}>Who's viewed your profile</Typography>
+            <Typography color={main} fontWeight="500">
+              {viewedProfile}
+            </Typography>
+          </FlexBetween>
+          <FlexBetween>
+            <Typography color={medium}>Impressions of your post</Typography>
+            <Typography color={main} fontWeight="500">
+              {impressions}
+            </Typography>
+          </FlexBetween>
+        </Box>
+
+        {/* FOURTH ROW */}
+        <Box p="1rem 0"></Box>
+      </FlexBetween>
+    </WidgetWrapper>
+  );
 };
 
 export default UserWidget;
