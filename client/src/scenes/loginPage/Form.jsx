b7ea8e775ce799e8946a9be3ee87046573bd6d73
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-03-25 15:53:31 +0200

    Separate constants for validations and values

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -14,37 +14,8 @@ import Dropzone from "react-dropzone";
 import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { setLogin } from "state";
-import * as yup from "yup";
-
-const registerSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  password: yup.string().required("required"),
-  location: yup.string().required("required"),
-  occupation: yup.string().required("required"),
-  picture: yup.string().required("required"),
-});
-
-const loginSchema = yup.object().shape({
-  email: yup.string().email("invalid email").required("required"),
-  password: yup.string().required("required"),
-});
-
-const initialValuesRegister = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  password: "",
-  location: "",
-  occupation: "",
-  picture: "",
-};
-
-const initialValuesLogin = {
-  email: "",
-  password: "",
-};
+import { registerSchema, loginSchema } from "./validationSchemas";
+import { initialValuesRegister, initialValuesLogin } from "./formValues";
 
 const Form = () => {
   const { palette } = useTheme();
