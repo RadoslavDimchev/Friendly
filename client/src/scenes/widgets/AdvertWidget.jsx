6456ffd7cf8204fd24776ee458d22126651f78a5
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-03-25 11:33:57 +0200

    Create and add also working Friend list widget

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -3,7 +3,7 @@ import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 
 const AdvertWidget = () => {
-  const palette = useTheme();
+  const { palette } = useTheme();
   const dark = palette.neutral.dark;
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
