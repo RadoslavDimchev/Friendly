5443366d28655a2a862975822e32697023c08a18
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-03-25 17:50:13 +0200

    Make links to work in navbar

--- client/src/scenes/navbar/navSubcomponents/NavIconsLinks.jsx
+++ client/src/scenes/navbar/navSubcomponents/NavIconsLinks.jsx
@@ -1,17 +1,31 @@
-import { Help, Message, Notifications } from "@mui/icons-material";
+import { AccountCircleOutlined, AddCircleOutline } from "@mui/icons-material";
+import { IconButton, useTheme } from "@mui/material";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavIconsLinks = () => {
   const isAuth = Boolean(useSelector((state) => state.token));
+  const user = useSelector((state) => state.user);
+  const theme = useTheme();
+  const dark = theme.palette.neutral.dark;
+
+  if (!isAuth) {
+    return null;
+  }
 
   return (
-    isAuth && (
-      <>
-        <Message sx={{ fontSize: "25px" }} />
-        <Notifications sx={{ fontSize: "25px" }} />
-        <Help sx={{ fontSize: "25px" }} />
-      </>
-    )
+    <>
+      <Link to="/create">
+        <IconButton>
+          <AddCircleOutline sx={{ color: dark, fontSize: "25px" }} />
+        </IconButton>
+      </Link>
+      <Link to={`/profile/${user._id}`}>
+        <IconButton>
+          <AccountCircleOutlined sx={{ color: dark, fontSize: "25px" }} />
+        </IconButton>
+      </Link>
+    </>
   );
 };
 
