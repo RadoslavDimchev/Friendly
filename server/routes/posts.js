79bb4b34f03b6338f2427ba6bc8f164002a58224
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-03-26 19:21:24 +0300

    Create request for one post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,6 +4,7 @@ import {
   getFeedPosts,
   getUserPosts,
   likePost,
+  getPost,
 } from '../controllers/posts.js';
 
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 // READ
 router.get('/', getFeedPosts);
 router.get('/:userId/posts', getUserPosts);
+router.get('/:postId', getPost);
 
 // UPDATE
 router.patch('/:id/like', verifyToken, likePost);
