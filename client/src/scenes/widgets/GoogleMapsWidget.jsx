121ab7a03c26cfc19d116bdd0cf219fde1a6f457
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-04-08 13:20:33 +0300

    Not showing google maps if user don't has coords

--- client/src/scenes/widgets/GoogleMapsWidget.jsx
+++ client/src/scenes/widgets/GoogleMapsWidget.jsx
@@ -1,22 +1,26 @@
-import { Box } from "@mui/material";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-import WidgetWrapper from "components/WidgetWrapper";
+import { Box } from '@mui/material';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import WidgetWrapper from 'components/WidgetWrapper';
 
 const containerStyle = {
-  width: "100%",
-  height: "40vh",
+  width: '100%',
+  height: '40vh',
 };
 
 const GoogleMapsWidget = ({ user }) => {
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: ["places"],
+    libraries: ['places'],
   });
 
   if (loadError) {
     return <Box>Map cannot be loaded right now, sorry.</Box>;
   }
-  const position = { lat: user?.coordinates?.lat, lng: user?.coordinates?.lng };
+  if (!user.coordinates) {
+    return null;
+  }
+
+  const position = { lat: user.coordinates.lat, lng: user.coordinates.lng };
   const hasPosition = !!position.lat;
   const defaultPosition = { lat: 41.562264, lng: 23.278419 };
 
@@ -31,7 +35,7 @@ const GoogleMapsWidget = ({ user }) => {
           {hasPosition && <Marker position={position} />}
         </GoogleMap>
       ) : (
-        "Loading..."
+        'Loading...'
       )}
     </WidgetWrapper>
   );
